2f603e7e0d21a1fbd8a783147844ab79e385e183
Skibidi50-lol <realhatroi@gmail.com>
2025-09-06 11:26:14 +0700

    Create groweggs.lua

---
+++ src/groweggs.lua
@@ -0,0 +1,73 @@
+--SKIDDER AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+   Name = "CherryWare - Grow Eggs",
+   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+   LoadingTitle = "CherryWare",
+   LoadingSubtitle = "by Cherry",
+   ShowText = "Rayfield", -- for mobile users to unhide rayfield, change if you'd like
+   Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+
+   ToggleUIKeybind = "K", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
+
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "Big Hub"
+   },
+
+   Discord = {
+      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
+      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+
+   KeySystem = false, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Untitled",
+      Subtitle = "Key System",
+      Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
+      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
+})
+
+local mainTab = Window:CreateTab("Main", 4483362458)
+
+local Section = mainTab:CreateSection("Duping")
+
+local Button = Tab:CreateButton({
+   Name = "Get Forerver Pack",
+   Callback = function()
+         local args = {
+                1
+            }
+
+            pcall(function()
+            game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BoughtForeverPack"):FireServer(unpack(args))
+        end)
+   end,
+})
+
+local Section = mainTab:CreateSection("Selling")
+
+local Button = Tab:CreateButton({
+   Name = "Sell All",
+   Callback = function()
+      local args = {
+                    "SellAllInventory",
+                    "9AD8C325-73F7-4BFA-AB4E-930F80A1DB52"
+                }
+
+                pcall(function()
+                    game:GetService("ReplicatedStorage"):WaitForChild("Postie"):WaitForChild("Sent"):FireServer(unpack(args))
+                end)  
+   end,
+})
